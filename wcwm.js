46ffead555b509112d8b484a58fa8224041342b8
HuzaifaaM007 <huzaifamurtaza007@gmail.com>
2026-01-16 02:26:36 +0500

    Local updated plugin files

---
+++ wcwm.js
@@ -0,0 +1,118 @@
+jQuery(document).ready(function ($) {
+
+    $('.wcwm-button').on('click', function () {
+
+        let name = $(this).data('product-name');
+        let price = $(this).data('product-price');
+        let sku = $(this).data('product-sku') ? $(this).data('product-sku') : 'N/A';
+        let url = $(this).data('product-url');
+        let phone = $(this).data('admin-phone-number');
+
+        let message = 'Hello\n';
+        message += 'I am interested in this product:\n\n';
+        message += 'Name: ' + name + '\n';
+        message += 'Price: Rs ' + price + '\n';
+        message += 'SKU: ' + sku + '\n';
+        message += 'Link: ' + url + '\n';
+
+        let whatsApp_url =
+            'https://wa.me/' + phone + '?text=' + encodeURIComponent(message);
+
+        window.open(whatsApp_url, '_blank');
+    });
+
+
+    $(document).on('click','.wcwm-checkout-button', function () {
+
+    // Safety check
+    if (typeof wcwm_cart_data === 'undefined' || !wcwm_cart_data.items.length) {
+        alert('Cart is empty');
+        return;
+    }
+
+    //  Read user checkout fields
+    let firstName = $('#billing_first_name').val() || '';
+    let lastName  = $('#billing_last_name').val() || '';
+    let phone     = $('#billing_phone').val() || '';
+    let email     = $('#billing_email').val() || '';
+    let address   = $('#billing_address_1').val() || '';
+    let city      = $('#billing_city').val() || '';
+    let notes     = $('#order_comments').val() || '';
+    let payment   = $('input[name="payment_method"]:checked').val() || '';
+
+    let message = 'Order Details\n\n';
+
+    //  Add user info
+    message += 'Customer Details:\n';
+    message += 'Name: ' + firstName + ' ' + lastName + '\n';
+    message += 'Phone: ' + phone + '\n';
+    message += 'Email: ' + email + '\n';
+    message += 'Address: ' + address + ', ' + city + '\n';
+    message += 'Payment Method: ' + payment + '\n';
+
+    if (notes) {
+        message += 'Order Notes: ' + notes + '\n';
+    }
+
+    message += '\n----------------------\n\n';
+
+    console.log("cart items : ", wcwm_cart_data);
+
+    wcwm_cart_data.items.forEach(function (item, index) {
+
+        console.log(item);
+
+        message += (index + 1) + '. ' + item.name + '\n';
+        message += 'Qty: ' + item.quantity + '\n';
+        message += 'Price: ' + wcwm_cart_data.currency + ' ' + item.price + '\n';
+        message += 'Subtotal: ' + wcwm_cart_data.currency + ' ' + item.subtotal + '\n\n';
+        message += 'tax: ' + wcwm_cart_data.currency + ' ' +item.tax+ '\n\n';  
+    });
+
+    message += 'Total: ' + wcwm_cart_data.currency + ' ' + wcwm_cart_data.total;
+
+    let admin_phone_number = wcwm_cart_data.admin_phone_number ;
+    let whatsapp_url =
+        'https://wa.me/' + admin_phone_number + '?text=' + encodeURIComponent(message);
+
+    window.open(whatsapp_url, '_blank');
+});
+
+//     $(document).on('click','.wcwm-checkout-button', function () {
+
+//         console.log("printing : " + wcwm_order);
+        
+//         if (typeof wcwm_order === 'undefined' || !wcwm_order.items.length) {
+//             alert('No items in order');
+//             return;
+//         }
+
+
+//         let message = 'Order Details\n\n';
+
+//         message += 'Order ID: '+ wcwm_order.order_id +'\n';
+//         message +=  'name: ' + wcwm_order.billing_name + '\n';
+//         message += 'email: ' + wcwm_order.billing_email + '\n';
+
+
+//         console.log("cart items : " , wcwm_order);
+        
+//         wcwm_order.items.forEach(function (item, index) {
+
+//          console.log(item);
+         
+//             message += (index + 1) + '. ' + item.name + '\n';
+//             message += 'Qty: ' + item.quantity + '\n';
+//             message += 'Price: ' + wcwm_order.currency  +' '+ item.price + '\n';
+//             message += 'Subtotal: ' + wcwm_order.currency +' '+ item.subtotal + '\n\n';
+//         });
+
+//         message += 'Total: ' + wcwm_order.currency + wcwm_order.total;
+
+//         let whatsapp_url =
+//             'https://wa.me/' + '+923149313252' + '?text=' + encodeURIComponent(message);
+
+//         window.open(whatsapp_url, '_blank');
+//     });
+
+});
---
+++ woocommerce-whatsapp-message.php
@@ -0,0 +1,343 @@
+<?php
+
+/**
+ * Plugin Name: woocommerce whatsapp message
+ * Plugin URI: https://example.com
+ * Description: A simple WooCommerce plugin that send the order details on whatsapp
+ * Version: 1.0
+ * Author: Huzaifa
+ * Author URI:
+ * Text Domain: woocommerce-whatsapp-message
+ */
+
+
+
+require_once plugin_dir_path(__FILE__) . 'admin/setting.php';
+
+
+// if accessed directly exit
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+function wcwm_is_woocommerce_active()
+{
+    return class_exists('WooCommerce');
+}
+
+
+function wcwm_wc_missing_notice()
+{
+    if (!wcwm_is_woocommerce_active()) {
+        echo '<div class="notice notice-error" >
+        <p><strong> first woocomerce plugin Message : </strong> requires Woo Commerce to be active </p>
+            </div>';
+    }
+}
+
+add_action('admin_notices', 'wcwm_wc_missing_notice');
+
+
+function wcwm_add_button_after_add_to_cart()
+{
+    if (!wcwm_is_woocommerce_active() || !is_product() ) {
+        return;
+    }
+
+    $product_id = get_the_ID();
+
+    $product = wc_get_product($product_id);
+
+    $wcwm_locations = get_option('wcwm_locations',[]);
+
+    if (!array($wcwm_locations)) {
+        return;
+    }
+
+    if (($wcwm_locations['detail'] ?? 'no') === 'yes') {
+        
+?>
+
+    <button
+        type="button"
+        class="wcwm-button button"
+        data-product-name="<?php echo esc_attr($product->get_name()); ?>"
+        data-product-price="<?php echo esc_attr($product->get_price()); ?>"
+        data-product-sku="<?php echo esc_attr($product->get_sku()); ?>"
+        data-product-url="<?php echo esc_url(get_permalink($product->get_id())); ?>"
+        data-admin-phone-number="<?php echo esc_attr(get_option('wcwm_admin_phone_number')); ?>">
+        Send to WhatsApp
+    </button>
+
+    <div id="wcwm-message" style="margin-top:10px;"></div>
+<?php
+
+    }
+    }
+
+add_action('woocommerce_after_add_to_cart_button', 'wcwm_add_button_after_add_to_cart');
+
+function wcwm_add_button_after_product_card()
+{
+
+    if (!wcwm_is_woocommerce_active()) {
+        return;
+    }
+
+    $product = wc_get_product(get_the_ID());
+
+    if (!$product) {
+        return;
+    }
+
+        $wcwm_locations = get_option('wcwm_locations',[]);
+
+    if (!array($wcwm_locations)) {
+        return;
+    }
+
+    if (($wcwm_locations['list'] ?? 'no') === 'yes') {
+
+?>
+
+    <button
+        type="button"
+        class="wcwm-button button"
+        data-product-name="<?php echo esc_attr($product->get_name()); ?>"
+        data-product-price="<?php echo esc_attr($product->get_price()); ?>"
+        data-product-sku="<?php echo esc_attr($product->get_sku()); ?>"
+        data-product-url="<?php echo esc_url(get_permalink($product->get_id())); ?>"
+        data-admin-phone-number="<?php echo esc_attr(get_option('wcwm_admin_phone_number')); ?>">
+        Send to WhatsApp
+    </button>
+
+    <div id="wcwm-message" style="margin-top:10px;"></div>
+<?php
+    }
+
+}
+
+add_action('woocommerce_after_shop_loop_item', 'wcwm_add_button_after_product_card');
+
+
+function wcwm_add_button_after_checkout()
+{
+
+    if (! wcwm_is_woocommerce_active()) {
+        return;
+    }
+
+        $wcwm_locations = get_option('wcwm_locations',[]);
+
+    if (!array($wcwm_locations)) {
+        return;
+    }
+
+    if (($wcwm_locations['checkout'] ?? 'no') === 'yes') {
+
+
+?>
+
+    <div style="margin-top:10px;">
+        <button
+            type="submit"
+            class="wcwm-checkout-button button"
+            style="margin-top: 10px;">
+            <!-- name="custom-order" -->
+
+
+            <!-- data-cart="<?php wc()->cart->get_cart() ?>" -->
+            <!-- data-cart-total="<?php wc()->cart->get_cart_total() ?>" -->
+            <!-- data-cart-subtotal="<?php wc()->cart->get_cart_subtotal() ?>" -->
+
+
+            CheckOut using WhatsApp</button>
+    </div>
+
+
+<?php
+    }
+}
+
+add_action("woocommerce_review_order_after_submit", "wcwm_add_button_after_checkout");
+
+
+
+function wcwm_order_data($order_id)
+{
+
+    if (isset($_POST['custom-order'])) {
+
+        $order = wc_get_order($order_id);
+
+
+        $billing_name = $order->get_billing_first_name() . '' . $order->get_billing_last_name();
+        $billing_email = $order->get_billing_email();
+
+
+        // order data
+
+        $items_data = [];
+
+        foreach ($order->get_items() as $item) {
+            $items_data[] = [
+                'name' => $item->get_name(),
+                'quantity' => $item->get_quantity(),
+                // 'total' => $item->get_total(),
+
+            ];
+        }
+
+        $total = $order->get_total();
+
+        wc()->session->set('wcwm_order_data', [
+            'order_id' => $order_id,
+            'billing_name' => $billing_name,
+            'billing_email' => $billing_email,
+            'items' => $items_data,
+            'total' => $total,
+        ]);
+    }
+}
+
+// add_action("woocommerce_checkout_order_processed", "wcwm_order_data");
+
+
+
+
+
+
+
+
+
+
+function wcwm_enqueue_scripts()
+{
+    // if (! is_product()) {
+    //     return;
+    // }
+
+    wp_enqueue_script(
+        'wcwm-js',
+        plugin_dir_url(__FILE__) . 'wcwm.js',
+        array('jquery'),
+        '1.0',
+        true
+
+    );
+}
+
+
+
+add_action('wp_enqueue_scripts', 'wcwm_enqueue_scripts');
+
+
+
+function wcwm_localize_cart_data()
+{
+    if (!is_checkout()) {
+        return;
+    }
+
+    if (wc()->cart->is_empty()) {
+        return;
+    }
+
+    $admin_phone_number = get_option('wcwm_admin_phone_number');
+
+    if (empty($admin_phone_number)) {
+        return;
+    }
+
+
+    $cart_items = [];
+
+
+    foreach (wc()->cart->get_cart() as $cart_item) {
+        $product = $cart_item['data'];
+
+
+        $cart_items[] = [
+            'name' => $product->get_name(),
+            'price' => $product->get_price(),
+            'quantity' => $cart_item['quantity'],
+            'subtotal' => $cart_item['line_total'],
+            'tax' => $cart_item['line_tax'],
+            'sku' => $product->get_sku(),
+            'url' => get_permalink($product->get_id()),
+        ];
+    }
+
+    wp_localize_script(
+        'wcwm-js',
+        'wcwm_cart_data',
+        [
+            'items' => $cart_items,
+            'total' => WC()->cart->get_total('edit'),
+            'currency' => get_woocommerce_currency_symbol(),
+            'admin_phone_number' => $admin_phone_number,
+        ]
+
+    );
+}
+
+add_action("wp_enqueue_scripts", "wcwm_localize_cart_data");
+
+function wcwm_localize_order_data()
+{
+
+    $data = wc()->session->get('wcwm_order_data');
+
+    if (!$data) {
+        return;
+    }
+
+    wp_localize_script(
+        'wcwm-js',
+        'wcwm_order',
+        $data
+
+    );
+}
+
+add_action("wp_enqueue_scripts", "wcwm_localize_order_data");
+
+
+// used for single product not usable for loop products
+
+
+// function wcwm_localize_scripts()
+// {
+//     // if (!is_product()) {
+//     //     return;
+//     // }
+
+//     // global $product;
+
+    // $product_id = get_the_ID();
+
+    // $product = wc_get_product($product_id);
+
+//     // if (! is_a($product, 'WC_Product')) return; // Ensure $product is object
+
+//     if (!$product) {
+//         return;
+//     }
+
+
+//     wp_localize_script(
+//         'wcwm-js',
+//         'wcwm_products_data',
+//         array(
+//             'name' => $product->get_name(),
+//             'price' => $product->get_price(),
+//             'sku' => $product->get_sku(),
+//             'url' => get_permalink($product->get_id()),
+//             'phone' => '+923149313252'
+
+//         )
+
+//     );
+// }
+
+// add_action('wp_enqueue_scripts', 'wcwm_localize_scripts');
